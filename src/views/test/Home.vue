6e4f89049be5c99512058493548eec47e33d31a5
xxingk <243557675@qq.com>
2021-01-27 10:27:39 +0800

    vuecli3 + elementui

---
+++ src/views/test/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home" style="font-size:400px">
+    test
+    <svg-icon icon-class="a"/>
+    <svg-icon icon-class="b"/>
+  </div>
+</template>
+
+<script>
+// import { mapActions, mapGetters } from 'vuex'
+// import { getList } from '@/request/api/api.js'
+// export default {
+//   name: 'test',
+//   data () {
+//     return {
+//       data: 'data',
+//       obj: {}
+//     }
+//   },
+//   mounted () {
+//     this.init()
+//   },
+//   computed: {
+//     ...mapGetters(['id'])
+//   },
+//   methods: {
+//     init () {
+//       let params = {
+//         page: '1',
+//         page_size: '20'
+//       }
+//       console.log(getList)
+//       getList(params).then((res) => {
+//         console.log('res')
+//         console.log(res)
+//       })
+//     },
+//     ...mapActions(['setId'])
+//   }
+// }
+</script>
